7ba4495bc28ac29765f38c71be7833c8713e6a25
DenisS374795 <ddenisskobelev37479@gmail.com>
2025-01-26 22:08:06 +0300

    Add files via upload

---
+++ Unit1.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+#include <string.h>
+using namespace std;
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+string oleg = "υσι";
+}
+//---------------------------------------------------------------------------
